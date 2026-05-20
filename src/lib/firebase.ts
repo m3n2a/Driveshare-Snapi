1e21ac70f85cdec0e857ed4ef881d6b7f85bdd50
M.N.M <m3ed7ardo@gmail.com>
2026-05-20 16:27:39 -0500

    Remove Firebase config from firebase.ts
    Removed sensitive Firebase configuration details.

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,14 +3,7 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDifecpzna9rAedEIlw0ImAYo53-cLIWl0",
-  authDomain: "snapi-3c80d.firebaseapp.com",
-  databaseURL: "https://snapi-3c80d-default-rtdb.firebaseio.com",
-  projectId: "snapi-3c80d",
-  storageBucket: "snapi-3c80d.firebasestorage.app",
-  messagingSenderId: "951911365298",
-  appId: "1:951911365298:web:7ca07d02244dec928b27ab",
-  measurementId: "G-0GS3L5LK90"
+
 
 };
 
